bca1c3eae767dc420fbb74473ccaa6d1566ebe66
priyam <priyamshankar.5@gmail.com>
2023-01-17 00:50:05 +0530

    rotate array

---
+++ day2/rotateArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        if(k==nums.size())return;
+        vector<int>v(nums.size(),0);
+
+        if(k>nums.size()){
+            k=k%nums.size();
+        }
+        int h=nums.size()-k;
+        for(int i=0; i<nums.size(); i++){
+            if(h>=nums.size())h=0;
+            v[i]=nums[h];
+            h++;
+        }
+        for(int i=0; i<nums.size();i++){
+            nums[i]=v[i];
+        }
+    }
+};
